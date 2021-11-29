cad06525ee3e6594a12c36defc517dfd2f83d6b4
Dmitry M <matovxd@gmail.com>
2021-11-29 23:30:43 +0300

    add trigger

--- project.sql
+++ project.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 14.1
 -- Dumped by pg_dump version 14.1
 
--- Started on 2021-11-29 18:55:08
+-- Started on 2021-11-29 23:29:39
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,6 +17,33 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- TOC entry 215 (class 1255 OID 16876)
+-- Name: calc_price(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.calc_price() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+declare
+	total money;
+	price money;
+	rent_dur smallint;
+	id_ int;
+begin
+	id_ = new.sklad_id;
+	rent_dur = new.rent_duration;
+	price = new.price_for_one_month;
+	total = rent_dur * price;
+	update skladi set total_price = total
+	where sklad_id = id_;
+	return new;
+end;
+$$;
+
+
+ALTER FUNCTION public.calc_price() OWNER TO postgres;
 
 SET default_tablespace = '';
 
@@ -90,7 +117,7 @@ CREATE TABLE public.skladi (
     total_space smallint NOT NULL,
     price_for_one_month money NOT NULL,
     rent_duration smallint NOT NULL,
-    total_price money NOT NULL
+    total_price money
 );
 
 
@@ -110,7 +137,7 @@ CREATE TABLE public.skladi_client (
 ALTER TABLE public.skladi_client OWNER TO postgres;
 
 --
--- TOC entry 3344 (class 0 OID 16854)
+-- TOC entry 3346 (class 0 OID 16854)
 -- Dependencies: 214
 -- Data for Name: all_users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -120,7 +147,7 @@ COPY public.all_users (user_id, is_admin) FROM stdin;
 
 
 --
--- TOC entry 3341 (class 0 OID 16824)
+-- TOC entry 3343 (class 0 OID 16824)
 -- Dependencies: 211
 -- Data for Name: client; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -130,7 +157,7 @@ COPY public.client (client_id, client_name, client_email) FROM stdin;
 
 
 --
--- TOC entry 3343 (class 0 OID 16844)
+-- TOC entry 3345 (class 0 OID 16844)
 -- Dependencies: 213
 -- Data for Name: items; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -140,7 +167,7 @@ COPY public.items (item_id, item_name, item_space, owner) FROM stdin;
 
 
 --
--- TOC entry 3339 (class 0 OID 16809)
+-- TOC entry 3341 (class 0 OID 16809)
 -- Dependencies: 209
 -- Data for Name: sklad_owner; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -150,7 +177,7 @@ COPY public.sklad_owner (owner_id, owner_name, owner_email) FROM stdin;
 
 
 --
--- TOC entry 3340 (class 0 OID 16814)
+-- TOC entry 3342 (class 0 OID 16814)
 -- Dependencies: 210
 -- Data for Name: skladi; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -160,7 +187,7 @@ COPY public.skladi (sklad_id, owner_id, free_space, total_space, price_for_one_m
 
 
 --
--- TOC entry 3342 (class 0 OID 16829)
+-- TOC entry 3344 (class 0 OID 16829)
 -- Dependencies: 212
 -- Data for Name: skladi_client; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -170,7 +197,7 @@ COPY public.skladi_client (sklad_id, client_id) FROM stdin;
 
 
 --
--- TOC entry 3195 (class 2606 OID 16859)
+-- TOC entry 3196 (class 2606 OID 16859)
 -- Name: all_users all_users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -179,7 +206,7 @@ ALTER TABLE ONLY public.all_users
 
 
 --
--- TOC entry 3189 (class 2606 OID 16828)
+-- TOC entry 3190 (class 2606 OID 16828)
 -- Name: client client_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -188,7 +215,7 @@ ALTER TABLE ONLY public.client
 
 
 --
--- TOC entry 3193 (class 2606 OID 16848)
+-- TOC entry 3194 (class 2606 OID 16848)
 -- Name: items items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -197,7 +224,7 @@ ALTER TABLE ONLY public.items
 
 
 --
--- TOC entry 3185 (class 2606 OID 16813)
+-- TOC entry 3186 (class 2606 OID 16813)
 -- Name: sklad_owner sklad_owner_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -206,7 +233,7 @@ ALTER TABLE ONLY public.sklad_owner
 
 
 --
--- TOC entry 3191 (class 2606 OID 16833)
+-- TOC entry 3192 (class 2606 OID 16833)
 -- Name: skladi_client skladi_client_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -215,7 +242,7 @@ ALTER TABLE ONLY public.skladi_client
 
 
 --
--- TOC entry 3187 (class 2606 OID 16818)
+-- TOC entry 3188 (class 2606 OID 16818)
 -- Name: skladi skladi_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -224,7 +251,15 @@ ALTER TABLE ONLY public.skladi
 
 
 --
--- TOC entry 3199 (class 2606 OID 16849)
+-- TOC entry 3201 (class 2620 OID 16879)
+-- Name: skladi calc_price; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER calc_price AFTER INSERT ON public.skladi FOR EACH ROW EXECUTE FUNCTION public.calc_price();
+
+
+--
+-- TOC entry 3200 (class 2606 OID 16849)
 -- Name: items items_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -233,7 +268,7 @@ ALTER TABLE ONLY public.items
 
 
 --
--- TOC entry 3198 (class 2606 OID 16839)
+-- TOC entry 3199 (class 2606 OID 16839)
 -- Name: skladi_client skladi_client_client_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -242,7 +277,7 @@ ALTER TABLE ONLY public.skladi_client
 
 
 --
--- TOC entry 3197 (class 2606 OID 16834)
+-- TOC entry 3198 (class 2606 OID 16834)
 -- Name: skladi_client skladi_client_sklad_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -251,7 +286,7 @@ ALTER TABLE ONLY public.skladi_client
 
 
 --
--- TOC entry 3196 (class 2606 OID 16819)
+-- TOC entry 3197 (class 2606 OID 16819)
 -- Name: skladi skladi_owner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -259,7 +294,7 @@ ALTER TABLE ONLY public.skladi
     ADD CONSTRAINT skladi_owner_id_fkey FOREIGN KEY (owner_id) REFERENCES public.sklad_owner(owner_id) ON DELETE CASCADE;
 
 
--- Completed on 2021-11-29 18:55:08
+-- Completed on 2021-11-29 23:29:40
 
 --
 -- PostgreSQL database dump complete
