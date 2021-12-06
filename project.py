f73d6529255da87312a92f6cbaab096007a82fe1
Dmitry M <matovxd@gmail.com>
2021-12-07 01:55:06 +0300

    add add_new_user function

--- project.py
+++ project.py
@@ -4,17 +4,37 @@ from psycopg2 import OperationalError
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+
 def create_connection():
-    connection = psycopg2.connect(user="postgres", password="04022002")
+    connection = psycopg2.connect(dbname="bd_project",
+                                  user="postgres",
+                                  password="runi124t",
+                                  host="localhost")
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     return connection
+
+
+def add_new_user(user_id_):
+    cursor.execute("SELECT user_id from all_users where user_id = %s ", (user_id_,))
+    find_user = cursor.fetchone()
+
+    if find_user == None:
+        cursor.execute("INSERT INTO all_users VALUES ( %s )", (user_id_,))
+
+    return True
+
 
 connection = create_connection()
 
 print(connection)
 
 cursor = connection.cursor()
-cursor.execute('create database project')
+
+user = add_new_user(345)
+
+cursor.execute("SELECT * FROM all_users")
+record = cursor.fetchall()
+print(record)
 
 cursor.close()
 connection.close()
