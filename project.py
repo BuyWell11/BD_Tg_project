22cfdd7637b70a091fc4974eebd7856197db685d
BuyWell <>
2021-12-21 20:28:44 +0300

    add func from sql

--- project.py
+++ project.py
@@ -1,76 +1,44 @@
-from sqlalchemy import create_engine
-from psycopg2 import OperationalError
+from sqlalchemy import create_engine, select
+from sqlalchemy.sql import func
+from psycopg2 import OperationalError, Error
+from config import password
 
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
+
 #connects to database
 def create_connection():
-    connection = psycopg2.connect(dbname="bd_project",
-                                  user="postgres",
-                                  password="runi124t",
-                                  host="localhost")
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    try:
+        # Подключиться к существующей базе данных
+        connection = psycopg2.connect(user="postgres",
+                                      password=password,
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="project")
+
+        print(connection)
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+
     return connection
 
 
-#adds new user if he hasn't existed
-def add_new_user(user_id_):
-    cursor.execute("SELECT user_id from all_users where user_id = %s ", (user_id_,))
-    find_user = cursor.fetchone()
-
-    if find_user == None:
-        cursor.execute("INSERT INTO all_users VALUES ( %s )", (user_id_,))
-
-    return True
+def add_new_user_(con, id):
+    cur = con.cursor()
+    s = cur.callproc('add_new_user', [id, False])
+    print(s)
+    con.commit()
+    row_count = cur.rowcount
+    print(row_count)
 
 
-#returns to client sum of his items' space
-def general_item_space(client_id):
-    cursor.execute("SELECT SUM(item_space) FROM items WHERE owner = %s", (client_id,))
-    return cursor.fetchone()
+def add_new_owner(con, id, name, email):
+    cur = con.cursor()
+    s = cur.callproc('add_new_owner', [id, name, email])
+    print(s)
+    con.commit()
 
 
-#returns to client available warehouses, which are suitable for his items
-def available_sklads(item_space_sum):
-    cursor.execute("SELECT * FROM skladi WHERE free_space >= %s", (item_space_sum,))
-    return cursor.fetchall()
-
-
-#returns to user info about all of his items
-def info_about_items(user_id_):
-    cursor.execute("SELECT * FROM items WHERE owner = %s", (user_id_,))
-    return cursor.fetchall()
-
-
-#returns to owner info about all of his warehouses
-def info_about_owner_sklads(owner_id_):
-    cursor.execute("SELECT * FROM skladi WHERE owner_id = %s", (owner_id_,))
-    return cursor.fetchall()
-
-
-#returns to client all of his contracts
-def info_about_client_contracts(client_id_):
-    cursor.execute("SELECT * FROM contracts WHERE client_id = %s", (client_id_,))
-    return cursor.fetchall()
-
-#returns to owner all of his contracts
-def info_about_owner_contracts(owner_id_):
-    cursor.execute("SELECT contract_id, contracts.sklad_id, client_id, rent_duration, total_price "
-                   "FROM contracts, skladi "
-                   "WHERE contracts.sklad_id = skladi.sklad_id AND owner_id = %s", (owner_id_,))
-    return cursor.fetchall()
-
-
-
-connection = create_connection()
-
-print(connection)
-
-cursor = connection.cursor()
-
-
-
-cursor.close()
-connection.close()
+def
